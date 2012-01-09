d963e9e46dd81ce03f7b5ffd0cf9cebf71df3f8f
mauricio barria <thiswallz@gmail.com>
2012-01-09 15:11:40 -0300

    clear all

--- modules/blocklayered/blocklayered.js
+++ modules/blocklayered/blocklayered.js
@@ -87,7 +87,7 @@ function formatCurrency(num) {
     for (var i = 0; i < Math.floor((num.length-(1+i))/3); i++)
     num = num.substring(0,num.length-(4*i+3))+'.'+
     num.substring(num.length-(4*i+3));
-    return (((sign)?'':'-') + '$' + num + '.' + cents);
+    return (((sign)?'':'-') + '$' + num);
 }
 function initSliders()
 {
