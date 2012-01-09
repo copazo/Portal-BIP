80106a66ba08cc838785963a5515898e3a34c70c
mauricio barria <thiswallz@gmail.com>
2012-01-09 18:55:22 -0300

    clear all

--- modules/blocklayered/blocklayered.js
+++ modules/blocklayered/blocklayered.js
@@ -219,7 +219,7 @@ function reloadContent(params_plus)
 	if ($('#selectPrductSort').length)
 	{
 		var splitData = $('#selectPrductSort').val().split(':');
-		data += '&orderby='+splitData[0]+'&orderwayXXX='+splitData[1];
+		data += '&orderby='+splitData[0]+'&orderway='+splitData[1];
 	}
 	
 	var slideUp = true;
