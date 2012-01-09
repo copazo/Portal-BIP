dbae7d374a564590b3acd8b353179d8a13eba3e4
mauricio barria <thiswallz@gmail.com>
2012-01-09 15:08:32 -0300

    clear all

--- modules/blocklayered/blocklayered.js
+++ modules/blocklayered/blocklayered.js
@@ -74,13 +74,27 @@ function addSlider(type, data, unit)
 		unit: unit
 	});
 }
-
+function formatCurrency(num) {
+    num = num.toString().replace(/\$|\,/g,'');
+    if(isNaN(num))
+    num = "0";
+    sign = (num == (num = Math.abs(num)));
+    num = Math.floor(num*100+0.50000000001);
+    cents = num%100;
+    num = Math.floor(num/100).toString();
+    if(cents<10)
+    cents = "0" + cents;
+    for (var i = 0; i < Math.floor((num.length-(1+i))/3); i++)
+    num = num.substring(0,num.length-(4*i+3))+','+
+    num.substring(num.length-(4*i+3));
+    return (((sign)?'':'-') + '$' + num + '.' + cents);
+}
 function initSliders()
 {
 	$(sliderList).each(function(i, slider){
 		$('#layered_'+slider['type']+'_slider').slider(slider['data']);
 		$('#layered_'+slider['type']+'_range').html(slider['unit']+$('#layered_'+slider['type']+'_slider').slider('values', 0)+
-			' - ' + $('#layered_'+slider['type']+'_slider').slider('values', 1 )+slider['unit']);
+			' - ' + slider['unit'] + formatCurrency($('#layered_'+slider['type']+'_slider').slider('values', 1 )) );
 	});
 }
 
